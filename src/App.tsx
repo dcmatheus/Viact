45357be178a196f57c9a12bf58f63d5833e30ef1
dcmatheus <mathws.costag@gmail.com>
2022-10-06 01:09:55 -0300

    feat: add and configure unplugin-icons library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import tw from 'twin.macro'
-
-const Base = tw.div`bg-black`
+import LogosReact from '~icons/logos/react'
+import LogosVitejs from '~icons/logos/vitejs'
+const Base = tw.div`bg-blue-900 flex`
 
 function App() {
   return (
     <Base>
       <h1 tw="text-3xl text-white font-bold underline">App</h1>
+      <LogosReact className="text-4xl" />
+      <LogosVitejs className="text-4xl" />
     </Base>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
   plugins: [
+    Icons({ compiler: 'jsx', jsx: 'react' }),
     react({
       babel: {
         plugins: [
