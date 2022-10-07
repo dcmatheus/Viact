fb2bf2d521073818c185b45d25373e37952a28ef
dcmatheus <mathws.costag@gmail.com>
2022-10-06 21:09:48 -0300

    feat: make the src part like the baseURL

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Icons from 'unplugin-icons/vite'
 import WindiCSS from 'vite-plugin-windicss'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [Icons({ compiler: 'jsx', jsx: 'react' }), WindiCSS(), react()]
+  plugins: [
+    Icons({ compiler: 'jsx', jsx: 'react' }),
+    WindiCSS(),
+    react(),
+    tsconfigPaths()
+  ]
 })
