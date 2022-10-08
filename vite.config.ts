6d8a362bcd3b613a92ed2b0730ef1c85790351e5
dcmatheus <mathws.costag@gmail.com>
2022-10-07 21:13:23 -0300

    feat: implement auto import

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,21 @@ import react from '@vitejs/plugin-react'
 import Icons from 'unplugin-icons/vite'
 import WindiCSS from 'vite-plugin-windicss'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
   plugins: [
     Icons({ compiler: 'jsx', jsx: 'react' }),
+    AutoImport({
+      dts: true,
+      include: [
+        /\.[tj]sx?$/ // .ts, .tsx, .js, .jsx
+      ],
+      eslintrc: {
+        enabled: true
+      },
+      imports: ['react']
+    }),
     WindiCSS(),
     react(),
     tsconfigPaths()
