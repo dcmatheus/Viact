978cfa208833649a90b1355261bb5ec958c6387b
dcmatheus <mathws.costag@gmail.com>
2022-10-16 22:26:17 -0300

    fix: remove bug that skipped the second command in case of error in the first
    The error occurred in scripts that ran two other scripts. The "test:all" for example.

--- cypress/component/App.cy.tsx
+++ cypress/component/App.cy.tsx
@@ -1,10 +1,7 @@
 import App from 'App'
 
-const counterSelector = '[data-cy=counter]'
-
 describe('App.cy.ts', () => {
   it('playground', () => {
     cy.mount(<App />)
-    cy.get(counterSelector).should('have.text', '0')
   })
 })
